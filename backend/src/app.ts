f78112b0663d077cab740e374aeb33c2ed77827e
Shaurya <omshaurya941@gmail.com>
2026-03-02 16:09:21 +0530

    Initialize Express application with CORS, body parsing, and error handling middleware

---
+++ backend/src/app.ts
@@ -0,0 +1,24 @@
+import express, {Application} from "express";
+import cors, {CorsOptions} from "cors";
+import cookieParser from "cookie-parser";
+import { errorHandler } from "./middlewares/error.middleware";
+
+const app: Application = express();
+
+const Origin = process.env.ORIGIN;
+
+const corsOptions: CorsOptions = {
+    origin: Origin ? [Origin, "http://localhost:5173"] : ["http://localhost:5173"],
+    credentials:true,
+    methods: ["GET", "POST", "DELETE", "PUT", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}
+
+app.use(cors(corsOptions))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.json({limit: "16kb"}))
+app.use(cookieParser())
+
+// Error Handler middleware
+app.use(errorHandler);
+export {app}
